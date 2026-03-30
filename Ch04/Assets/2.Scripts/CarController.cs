d6b4ae5f564cf26bea234a8fff73ce4ca0c0d51c
KimJinPyo1 <jyfct5646@gmail.com>
2026-03-30 10:53:02 +0900

    0330

---
+++ Ch04/Assets/2.Scripts/CarController.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0.2f;
+    Vector2 startPos;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLen = endPos.x - startPos.x;
+            speed = swipeLen / 500f;
+        }
+            transform.Translate(speed, 0, 0);
+        speed *= 0.99f;
+    }
+}
