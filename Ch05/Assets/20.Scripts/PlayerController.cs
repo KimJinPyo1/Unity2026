3d619077abf26325ed4f9d171e66933b6ad0c9b2
sungharin <jyfct5646@gmail.com>
2026-04-13 10:51:00 +0900

    0413

--- Ch05/Assets/20.Scripts/PlayerController.cs
+++ Ch05/Assets/20.Scripts/PlayerController.cs
@@ -1,23 +1,37 @@
-using TreeEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 1f;
     void Start()
     {
         Application.targetFrameRate = 60;
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-speed, 0, 0);
-        }
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(speed, 0, 0);
-        }
+        
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.LeftArrow))
+    //    {
+    //        transform.Translate(-speed, 0, 0);
+    //    }
+    //    if(Input.GetKey(KeyCode.RightArrow))
+    //    {
+    //        transform.Translate(speed, 0, 0);
+    //    }
+    //}
+
+    public void LButtonDown()
+    {
+        transform.Translate(-speed, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(speed, 0, 0);
     }
 }
