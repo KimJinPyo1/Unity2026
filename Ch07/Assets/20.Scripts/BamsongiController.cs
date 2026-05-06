b403e1b0a4483e59361f5110be047a4152cd6f72
sungharin <jyfct5646@gmail.com>
2026-05-06 15:45:16 +0900

    0506

--- Ch07/Assets/20.Scripts/BamsongiController.cs
+++ Ch07/Assets/20.Scripts/BamsongiController.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class BamsongiController : MonoBehaviour
 {
+    public Renderer renderer;
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0, 200, 2000));
+        //Shoot(new Vector3(0, 200, 2000));
     }
 
     void Update()
@@ -16,5 +17,13 @@ public class BamsongiController : MonoBehaviour
     public void Shoot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        renderer.enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+        Destroy(gameObject, 0.5f);
     }
 }
---
+++ Ch07/Assets/20.Scripts/BamsongiGenerator.cs
@@ -0,0 +1,32 @@
+using Tanks.Complete;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    public GameObject bamsongiPrefab;
+    public float throwForce = 10f;
+
+    float startY;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startY = Input.mousePosition.y;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            GameObject bamsoni = Instantiate(bamsongiPrefab);
+            bamsoni.transform.position = transform.position;
+
+            float power = Input.mousePosition.y - startY;
+
+            Vector3 dir = transform.forward + transform.up * 0.5f;
+            bamsoni.GetComponent<BamsongiController>().Shoot(dir * power * throwForce);
+
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //bamsoni.GetComponent<BamsongiController>().Shoot(ray.direction * 2000);
+        }
+    }
+}
